7aee1d485de0e56ac9f358422ec66e84fffd8485
matheus.pessoa <matheus.pessoa@go.senac.br>
2023-10-30 10:41:22 -0300

    Adicionar arquivos de projeto.

---
+++ copaSescUser/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace copaSescUser.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet(Name = "Teste")]
+        public string Valores(string valores)
+        {
+            return "Ola";
+        }
+    }
+}
